316b28d34fa6404c72c278835db991b9a3919ba7
root <root@graderbackend-deployment-656475d4cb-rb4wb>
2019-04-29 14:34:59 +0000

    Initial seed [ci skip]

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * The main class for your arcade game.
+ * 
+ * You can design your game any way you like, but make the game start
+ * by running main here.
+ * 
+ * Also don't forget to write javadocs for your classes and methods.
+ * 
+ * @author <YOUR_NAMES_GO_HERE>
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Begin your arcade game program here!");
+	}
+
+}
